6c8040cb6c167f7474308f64d55da024a64f6f09
Veliz Murrieta <avelizmu@mdc.edu>
2021-06-22 15:14:24 -0400

    Add scanner functionality to ShortAnswerQuestion

--- src/components/ShortAnswerQuestion/ShortAnswerQuestion.tsx
+++ src/components/ShortAnswerQuestion/ShortAnswerQuestion.tsx
@@ -1,20 +1,33 @@
 import styles from './ShortAnswerQuestion.module.css';
 import {useState} from "react";
+import Scanner from "../Scanner/Scanner";
 
 export default function ShortAnswerQuestion(args: {prompt: string, onConfirm: (answer: string) => void}) {
 
     const [answer, setAnswer] = useState('');
+    const [scanning, setScanning] = useState(false);
+    const [noDevicesFound, setNoDevicesFound] = useState(false);
 
-    return <div className={styles.container}>
-        <div className={styles.prompt}>
-            {args.prompt}
+    return scanning ?
+        <Scanner onScan={(answer) => {
+            if(scanning) {
+                setScanning(false);
+                setAnswer(answer);
+            }
+        }} onNoDevicesFound={() => setNoDevicesFound(true)}/> :
+        <div className={styles.container}>
+            <div className={styles.prompt}>
+                {args.prompt}
+            </div>
+            <div></div>
+            <div className={styles.inputContainer}>
+                <input className={styles.input} value={answer} onChange={(e) => setAnswer(e.target.value)}/>
+                {
+                    !noDevicesFound && <button className={styles.scanButton} onClick={() => setScanning(true)}>🔍</button>
+                }
+            </div>
+            <div className={styles.confirmContainer}>
+                <button className={styles.confirmButton} onClick={() => args.onConfirm(answer)}>Confirm</button>
+            </div>
         </div>
-        <div></div>
-        <div className={styles.inputContainer}>
-            <input className={styles.input} onChange={(e) => setAnswer(e.target.value)}/>
-        </div>
-        <div className={styles.confirmContainer}>
-            <button className={styles.confirmButton} onClick={() => args.onConfirm(answer)}>Confirm</button>
-        </div>
-    </div>
 }
