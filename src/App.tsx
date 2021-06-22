42a255bf91a067a3ee3ca9b5e37682af6d091c77
Veliz Murrieta <avelizmu@mdc.edu>
2021-06-22 13:21:18 -0400

    Add routing in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Form from "./pages/Form/Form"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/form/:formId">
+        <Form/>
+      </Route>
+    </Switch>
+  </BrowserRouter>
 }
-
-export default App;
