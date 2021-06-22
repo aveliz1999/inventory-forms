c7ddcc0539d290c2c7a2e1b65a3c68959cc2d528
Veliz Murrieta <avelizmu@mdc.edu>
2021-06-22 13:19:03 -0400

    Update YesNoQuestion to use its onSelect

--- src/components/YesNoQuestion/YesNoQuestion.tsx
+++ src/components/YesNoQuestion/YesNoQuestion.tsx
@@ -1,14 +1,14 @@
 import styles from './YesNoQuestion.module.css';
 
-export default function YesNoQuestion(args: {prompt: string}) {
+export default function YesNoQuestion(args: {prompt: string, onSelect: (selected: 'yes' | 'no') => void}) {
     return <div className={styles.container}>
         <div className={styles.prompt}>
             {args.prompt}
         </div>
-        <div className={styles.yes}>
+        <div className={styles.yes} onClick={() => args.onSelect('yes')}>
             Yes
         </div>
-        <div className={styles.no}>
+        <div className={styles.no} onClick={() => args.onSelect('no')}>
             No
         </div>
     </div>
